64c9d90d1d1ed59af489c7be50cea5a0aa6be3b7
abdulladone94 <abdullaf@booleanlabs.biz>
2023-07-29 08:10:38 +0530

    find me residence initial commit

---
+++ components/index/landing.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="px-5 text-white sm:px-16 lg:px-28">
+      <h1 className="text-4xl lg:text-5xl">
+        We are open for business, its just our web is not ready yet
+      </h1>
+      <div className="mt-10 text-xl">
+        <p>
+          We are a marketplace for buyers and sellers in real estate. We are
+          uniques and one of a kind startup in the industry. <br /> So our
+          founders believe the interaction with you in first hand is a great
+          first step to build absolutely phenomenal a human like tech, so no one
+          will feel that have not been served.
+          <br /> Until we get our platform up and running, you can reach us
+          through our socials, we can still sell your real estate way faster.
+        </p>
+        <p className="mt-10">Thank you for trusting us!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
